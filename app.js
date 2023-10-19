bf1b73c7802bcb2869bd3d5d3a5cdd0e5bfa2b18
KEIKO\USER <keitakeiko@hotmail.com>
2023-10-19 13:57:14 +0800

    feat: roughly add file and folder

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require(express)
 const app = express()
+const exphbs = require('express-handlebars')
+const bcrypt = require('bcryptjs')
+const methodOverride = require('method-override')
 
 const PORT = 3000
 
-app.use('/', (req, res) => res.send('hello world'))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => res.send('hello world'))
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
